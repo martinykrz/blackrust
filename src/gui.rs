32d16c3d92f7c37f2ebab4a66008844d255fc3df
martinykrz <ymartin014@gmail.com>
2023-07-18 00:05:51 -0300

    Card on center, TODO: Make union with logic

--- src/gui.rs
+++ src/gui.rs
@@ -3,47 +3,80 @@ use ggez::{
     glam::Vec2,
     conf::WindowMode,
     graphics::{self, Color, DrawParam},
+    // input::keyboard::{KeyCode, KeyInput},
     Context, GameResult
 };
+// use rand::prelude::*;
 use std::{
     env,
     path
 };
 
 struct MainState {
-    image: graphics::Image
+    image: graphics::Image,
+    dst: Vec2,
+    scale: Vec2,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let image = graphics::Image::from_path(
-            ctx, 
-            "/ace_spades.png"
-        )?;
+        let image = graphics::Image::from_path(ctx, "/ace_spades.png")?;
+        let scale = Vec2::new(0.10, 0.10);
+        let dst = Vec2::new(0.0, 0.0);
         let state = MainState {
             image,
+            dst,
+            scale
         };
         Ok(state)
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let (width, height) = ctx.gfx.size();
+        let pos_x = (width - (self.image.clone().width() as f32) * self.scale.x) / 2.0;
+        let pos_y = (height - (self.image.clone().height() as f32) * self.scale.y) / 2.0;
+        self.dst = Vec2::new(pos_x, pos_y);
         Ok(())
     }
+
+    /* fn key_down_event(&mut self, ctx: &mut Context, input: KeyInput, _repeat: bool) -> GameResult {
+        let suits = vec!["clubs", "diamonds", "hearts", "spades"];
+        let random = random::<usize>() % suits.clone().len();
+        let card = match input.keycode {
+            Some(KeyCode::Key2) => Some(format!("/02_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key3) => Some(format!("/03_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key4) => Some(format!("/04_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key5) => Some(format!("/05_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key6) => Some(format!("/06_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key7) => Some(format!("/07_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key8) => Some(format!("/08_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Key9) => Some(format!("/09_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::T) => Some(format!("/10_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::J) => Some(format!("/jack_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::Q) => Some(format!("/queen_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::K) => Some(format!("/king_{}.png", suits.get(random).unwrap())),
+            Some(KeyCode::A) => Some(format!("/ace_{}.png", suits.get(random).unwrap())),
+            _ => None
+        };
+        match card {
+            Some(c) => self.image = graphics::Image::from_path(ctx, c)?,
+            None => self.image = self.image.clone(),
+        }
+        Ok(())
+    } */
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(
             ctx,
             Color::from([0.1, 0.2, 0.3, 1.0])
         );
-        let dst = Vec2::new(20.0, 20.0);
-        let scl = Vec2::new(0.25, 0.25);
         canvas.draw(
             &self.image,
             DrawParam::new()
-                .dest(dst)
-                .scale(scl)
+                .dest(self.dst)
+                .scale(self.scale)
         );
         canvas.finish(ctx)?;
         Ok(())
@@ -58,11 +91,11 @@ pub fn test_gui() -> GameResult {
     } else {
         path::PathBuf::from("./resources")
     };
-    let mut win_mode = WindowMode::default();
-    win_mode.width = 1020.0;
-    win_mode.height = 720.0;
     let cb = ggez::ContextBuilder::new("card", "martinykrz")
-        .window_mode(win_mode)
+        .window_mode(
+            WindowMode::default()
+                .resizable(false)
+        )
         .add_resource_path(assets_dir);
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
